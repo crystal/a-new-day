3b3e453e3c8f944a7dba6dc5458d93b25dd56ff6
Crystal Tate <crys.tate@icloud.com>
2017-05-31 22:38:49 -0500

    Fixed errors

--- src/server.js
+++ src/server.js
@@ -1,26 +1,26 @@
 import bodyParser from 'body-parser';
 import express from 'express';
 
-import { handler } from '.'
+import { handler } from '.';
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.post('*', function (req, res) {
+app.post('*', (req, res) => {
   res.setHeader('content-type', 'application/json;charset=UTF-8');
   console.log(req.body);
   handler(req.body, {}, (err, body) => {
-    res.send(body)
+    res.send(body);
   });
 });
 
-app.get('*', function (req, res) {
+app.get('*', (req, res) => {
   res.setHeader('content-type', 'application/json;charset=UTF-8');
   res.send({});
 });
 
-app.listen(3000, '0.0.0.0', function () {
+app.listen(3000, '0.0.0.0', () => {
   console.log('Example app listening on port 3000!');
 });
